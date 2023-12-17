f638889a3bf8be11c575262db17dbfb25dbb5295
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-12-16 23:13:10 -0300

    Update status code in getTechnologiesHandler.js

--- src/handlers/getTechnologiesHandler.js
+++ src/handlers/getTechnologiesHandler.js
@@ -3,7 +3,7 @@ const getTechnologies = require('../controllers/getTechnologies')
 module.exports = async (req, res) => {
   try {
     const response = await getTechnologies()
-    res.status(500).json(response)
+    res.status(200).json(response)
   } catch (error) {
     res.status(404).json({ error: error.message })
   }
